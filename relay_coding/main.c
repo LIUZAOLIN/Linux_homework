3e2e8bba53a13dfc98e7d17ec5c359787bbe82bb
LIUZAOLIN <https://LIUZAOLIN@github.com>
2020-05-20 18:16:56 +0900

    update main.c

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -5,6 +5,6 @@ int main(){
 	printf("i hate this world!\n");
 	printf("bye world!\n");
 	printf("I hate corona!");
-	printf("My name is Liu Zaolin")
+	printf("My name is Liu Zaolin\n")
 	return 1;
 }
